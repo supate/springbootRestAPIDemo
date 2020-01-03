c441af9dd858e9030356f7a7a21b8f67a4f70597
sunipate <sunil.patel@oracle.com>
2020-01-03 21:40:17 +0530

    first commit in firstBranch

--- SpringBootHibernateJpaApplication.java
+++ SpringBootHibernateJpaApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class SpringBootHibernateJpaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Inside main method");
 		SpringApplication.run(SpringBootHibernateJpaApplication.class, args);
 	}
 }
